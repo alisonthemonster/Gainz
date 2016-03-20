a0abfc12494d634111a31ff0d62b51a8d266ffe7
alisonthemonster <alison.wyllie@gmail.com>
2016-03-19 22:08:13 -0500

    popup only appears when cell hasn't been rated and rating is now updated in parse SELECTED CELL STILL INCORRECT

--- Gainz/TodaysWorkoutTableViewController.swift
+++ Gainz/TodaysWorkoutTableViewController.swift
@@ -14,12 +14,13 @@ class TodaysWorkoutTableViewController: PFQueryTableViewController, ReloadViewDe
     
     var currentWorkout:PFObject?
     var alertController:UIAlertController? = nil
-    
 
     override func queryForTable() -> PFQuery {
         print("query for table")
+        //get the unsaved workout (today's workout)
         let innerQuery = PFQuery(className: "Workout")
         innerQuery.whereKey("saved", equalTo: false)
+        //get the exercises that are in unsaved workouts
         let query = PFQuery(className: "Exercise")
         query.whereKey("workout", matchesQuery: innerQuery)
         innerQuery.findObjectsInBackgroundWithBlock { (objects: [PFObject]?, error: NSError?) -> Void in
@@ -54,53 +55,108 @@ class TodaysWorkoutTableViewController: PFQueryTableViewController, ReloadViewDe
         cell.nameLabel.text = object?.objectForKey("name") as? String
         cell.exercise = object
         
+        if let rating = (object?.objectForKey("rating") as? Int) {
+            if (rating==0) {
+                cell.backgroundColor = UIColor.greenColor()
+            } else if (rating==1) {
+                cell.backgroundColor = UIColor.yellowColor()
+            } else if (rating==2) {
+                cell.backgroundColor = UIColor.redColor()
+            }
+        } else {
+            cell.backgroundColor = UIColor.whiteColor()
+        }
+        
         return cell
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        print("dequeing cell")
+        print(String(indexPath.row))
         let cell = tableView.dequeueReusableCellWithIdentifier("todayCell", forIndexPath: indexPath) as! TodaysWorkoutExerciseCell
         
         print("selected cell is: " + cell.nameLabel.text!)
+        //TODO: THE SELECTED CELL IS WRONG!!!
         
-        if (!cell.complete) {
-            //cell has already been rated
+        print(String(cell.backgroundColor))
+        if (cell.backgroundColor == UIColor(red: 1, green: 1, blue: 1, alpha: 1)) {
+            //cell has not yet been rated
             print("creating alert controller")
             self.alertController = UIAlertController(title: "Rate this workout", message: "Was this workout easy, medium, or hard? We'll plan your next workout based on your feedback.", preferredStyle: UIAlertControllerStyle.Alert)
             
             let buttonOne = UIAlertAction(title: "Easy", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
                 print("Button One Pressed")
+                print("changing this cell's color: " + cell.nameLabel.text!)
                 cell.backgroundColor = UIColor.greenColor()
                 cell.complete = true
-                //TODO
-                //update parse with new sets/reps/weight based on the easy setting
+                
+//                //update the rating in parse!
+//                var query = PFQuery(className:"Exercise")
+//                query.whereKey("name", equalTo: cell.nameLabel.text!)
+//                //TODO currently relies on the fact that exercise names are unique
+//                query.findObjectsInBackgroundWithBlock { (objects: [PFObject]?, error: NSError?) -> Void in
+//                    if (error != nil) {
+//                        print (error)
+//                    } else {
+//                        let object = objects![0]
+//                        object["rating"] = 0
+//                        object.saveInBackground()
+//                    }
+//                }
+
                 tableView.reloadData()
             })
             let buttonTwo = UIAlertAction(title: "Medium", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
                 print("Button Two Pressed")
+                print("changing this cell's color: " + cell.nameLabel.text!)
                 cell.backgroundColor = UIColor.yellowColor()
                 cell.complete = true
-                //TODO
-                //update parse with new sets/reps/weight based on the med setting
+                
+//                //update the rating in parse!
+//                var query = PFQuery(className:"Exercise")
+//                query.whereKey("name", equalTo: cell.nameLabel.text!)
+//                //TODO currently relies on the fact that exercise names are unique
+//                query.findObjectsInBackgroundWithBlock { (objects: [PFObject]?, error: NSError?) -> Void in
+//                    if (error != nil) {
+//                        print (error)
+//                    } else {
+//                        let object = objects![0]
+//                        object["rating"] = 1
+//                        object.saveInBackground()
+//                    }
+//                }
+                
                 tableView.reloadData()
             })
             let buttonThree = UIAlertAction(title: "Hard", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
                 print("Button Three Pressed")
+                print("changing this cell's color: " + cell.nameLabel.text!)
                 cell.backgroundColor = UIColor.redColor()
                 cell.complete = true
-                //TODO
-                //update parse with new sets/reps/weight based on the hard setting
+                
+//                //update the rating in parse!
+//                var query = PFQuery(className:"Exercise")
+//                query.whereKey("name", equalTo: cell.nameLabel.text!)
+//                //TODO currently relies on the fact that exercise names are unique
+//                query.findObjectsInBackgroundWithBlock { (objects: [PFObject]?, error: NSError?) -> Void in
+//                    if (error != nil) {
+//                        print (error)
+//                    } else {
+//                        let object = objects![0]
+//                        object["rating"] = 2
+//                        object.saveInBackground()
+//                    }
+//                }
+                
                 tableView.reloadData()
             })
             let buttonCancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (action) -> Void in
                 print("Cancel Button Pressed")
             }
-            print("adding buttons")
             self.alertController!.addAction(buttonOne)
             self.alertController!.addAction(buttonTwo)
             self.alertController!.addAction(buttonThree)
             self.alertController!.addAction(buttonCancel)
-            print("presenting popover")
+
             presentViewController(self.alertController!, animated: true, completion: nil)
         }
         
