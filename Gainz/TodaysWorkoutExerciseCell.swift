490ba858ed47e1bfc08a4363bac015c72a552745
alisonthemonster <alison.wyllie@gmail.com>
2016-03-19 19:59:47 -0500

    changing the edit texts to labels

--- Gainz/TodaysWorkoutExerciseCell.swift
+++ Gainz/TodaysWorkoutExerciseCell.swift
@@ -13,60 +13,18 @@ import ParseUI
 class TodaysWorkoutExerciseCell: PFTableViewCell, UITextFieldDelegate {
 
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var setsField: UITextField!
-    @IBOutlet weak var repsField: UITextField!
-    @IBOutlet weak var weightField: UITextField!
+    @IBOutlet weak var sets: UILabel!
+    @IBOutlet weak var reps: UILabel!
+    @IBOutlet weak var weights: UILabel!
+    
+    
     var exercise:PFObject?
     var complete:Bool = false
     
-    override func awakeFromNib() {
-        self.setsField.delegate = self
-        self.repsField.delegate = self
-        self.weightField.delegate = self
-        self.setsField.keyboardType = UIKeyboardType.NumbersAndPunctuation
-        self.repsField.keyboardType = UIKeyboardType.NumbersAndPunctuation
-        self.weightField.keyboardType = UIKeyboardType.NumbersAndPunctuation
-    }
+//    override func awakeFromNib() {
+//        self.setsField.delegate = self
+//        self.repsField.delegate = self
+//        self.weightField.delegate = self
+//    }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    // Saves exercise data of an exercise as the user finishes editing on any textField
-    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
-        setTextFieldInt(setsField, key: "sets")
-        setTextFieldInt(repsField, key: "reps")
-        setTextFieldInt(weightField, key: "weight")
-        exercise?.saveInBackgroundWithBlock{ (success: Bool, error: NSError?) -> Void in
-            if (success) {
-                print ("successful object save")
-            }
-            else {
-                print ("it failed")
-            }
-        }
-        return true
-    }
-    
-    // Checks that all fields are filled with appropriate data
-    func completed() -> Bool {
-        if (!isValidNum(setsField.text!) || repsField.text!.isEmpty || weightField.text!.isEmpty) {
-            complete = false
-        } else {
-            complete = true
-        }
-        return complete
-    }
-    
-    func isValidNum(str: String) -> Bool {
-        return !str.isEmpty && Int(str) != nil
-    }
-    
-    func setTextFieldInt(textField: UITextField, key: String) {
-        if let num = Int(textField.text!) {
-            exercise?[key] = num
-        }
-        exercise?.saveEventually()
-    }
-}
+   }
--- Gainz/TodaysWorkoutTableViewController.swift
+++ Gainz/TodaysWorkoutTableViewController.swift
@@ -13,8 +13,11 @@ import ParseUI
 class TodaysWorkoutTableViewController: PFQueryTableViewController, ReloadViewDelegate {
     
     var currentWorkout:PFObject?
+    var alertController:UIAlertController? = nil
+    
 
     override func queryForTable() -> PFQuery {
+        print("query for table")
         let innerQuery = PFQuery(className: "Workout")
         innerQuery.whereKey("saved", equalTo: false)
         let query = PFQuery(className: "Exercise")
@@ -31,10 +34,11 @@ class TodaysWorkoutTableViewController: PFQueryTableViewController, ReloadViewDe
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell? {
         let cell = tableView.dequeueReusableCellWithIdentifier("todayCell", forIndexPath: indexPath) as! TodaysWorkoutExerciseCell
+        print("loading cell!!!")
         
-        setTextField(cell.setsField, key: "sets", object: object)
-        setTextField(cell.repsField, key: "reps", object: object)
-        setTextField(cell.weightField, key: "weight", object: object)
+        cell.sets.text = object?.objectForKey("sets") as? String
+        cell.reps.text = object?.objectForKey("reps") as? String
+        cell.weights.text = object?.objectForKey("weight") as? String
         cell.nameLabel.text = object?.objectForKey("name") as? String
         cell.exercise = object
         
@@ -42,48 +46,54 @@ class TodaysWorkoutTableViewController: PFQueryTableViewController, ReloadViewDe
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        var alertController = UIAlertController()
-        
+        print("dequeing cell")
         let cell = tableView.dequeueReusableCellWithIdentifier("todayCell", forIndexPath: indexPath) as! TodaysWorkoutExerciseCell
-        alertController = UIAlertController(title: "Alert Controller", message: "Alert Controller with multiple buttons", preferredStyle: UIAlertControllerStyle.Alert)
         
-        let buttonOne = UIAlertAction(title: "Easy", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
-            print("Button One Pressed")
-            cell.backgroundColor = UIColor.greenColor()
-            tableView.reloadData()
-        })
-        let buttonTwo = UIAlertAction(title: "Medium", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
-            print("Button Two Pressed")
-            cell.backgroundColor = UIColor.yellowColor()
-            tableView.reloadData()
-        })
-        let buttonThree = UIAlertAction(title: "Hard", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
-            print("Button Three Pressed")
-            cell.backgroundColor = UIColor.redColor()
-            tableView.reloadData()
-        })
-        let buttonCancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (action) -> Void in
-            print("Cancel Button Pressed")
+        print("selected cell is: " + cell.nameLabel.text!)
+        
+        if (!cell.complete) {
+            //cell has already been rated
+            print("creating alert controller")
+            self.alertController = UIAlertController(title: "Rate this workout", message: "Was this workout easy, medium, or hard? We'll plan your next workout based on your feedback.", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let buttonOne = UIAlertAction(title: "Easy", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                print("Button One Pressed")
+                cell.backgroundColor = UIColor.greenColor()
+                cell.complete = true
+                //TODO
+                //update parse with new sets/reps/weight based on the easy setting
+                tableView.reloadData()
+            })
+            let buttonTwo = UIAlertAction(title: "Medium", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                print("Button Two Pressed")
+                cell.backgroundColor = UIColor.yellowColor()
+                cell.complete = true
+                //TODO
+                //update parse with new sets/reps/weight based on the med setting
+                tableView.reloadData()
+            })
+            let buttonThree = UIAlertAction(title: "Hard", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                print("Button Three Pressed")
+                cell.backgroundColor = UIColor.redColor()
+                cell.complete = true
+                //TODO
+                //update parse with new sets/reps/weight based on the hard setting
+                tableView.reloadData()
+            })
+            let buttonCancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (action) -> Void in
+                print("Cancel Button Pressed")
+            }
+            print("adding buttons")
+            self.alertController!.addAction(buttonOne)
+            self.alertController!.addAction(buttonTwo)
+            self.alertController!.addAction(buttonThree)
+            self.alertController!.addAction(buttonCancel)
+            print("presenting popover")
+            presentViewController(self.alertController!, animated: true, completion: nil)
         }
-        
-        alertController.addAction(buttonOne)
-        alertController.addAction(buttonTwo)
-        alertController.addAction(buttonThree)
-        alertController.addAction(buttonCancel)
-        
-        presentViewController(alertController, animated: true, completion: nil)
-
-        
         
     }
     
-    func setTextField (textField: UITextField, key: String, object: PFObject?) {
-        if let sets = (object?.objectForKey(key) as? Int) {
-            textField.text = String(sets)
-        } else {
-            textField.text = ""
-        }
-    }
     
     override func scrollViewWillBeginDragging(scrollView: UIScrollView) {
         self.view.endEditing(true)
